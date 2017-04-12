6d6d5116802df106aee9495f9e490845bdc062aa
Luis Fernando <lewis.cast@gmail.com>
2017-04-12 10:10:33 -0300

    inicio do controle

--- ConverStore/src/main/java/br/com/converstore/controller/IndexController.java
+++ ConverStore/src/main/java/br/com/converstore/controller/IndexController.java
@@ -1,5 +1,22 @@
 package br.com.converstore.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("index")
 public class IndexController {
+	
+	@RequestMapping("/main")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+
+	@RequestMapping("/cadprodutos")
+	public ModelAndView cadprodutos() {
+		
+		return new ModelAndView("cadprodutos");
+	}
 
 }
